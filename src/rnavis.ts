eb3e57061b9b7d4b62421f6403dd40db301e74db
Michal Hercík <hercik77@seznam.cz>
2022-07-31 10:27:03 +0200

    displaying on canvas

--- src/rnavis.ts
+++ src/rnavis.ts
@@ -6,29 +6,40 @@ import zoom from './zoom';
 
 export class RNAVis {
     private margin = 10;
-    private svg;    
+    private canvas;    
     private data;
+    private dataContainer;
 
     constructor(element: HTMLElement, data: RNAData) {
         this.data = data;
-        this.svg = d3.select(element)
-        .append('svg')
+        this.canvas = d3.select(element)
+        .append('canvas')
+        this.dataContainer = d3.select(document.createElement('custom'));
         this.setDimensions();
-    }
 
-    public addZoom() {
-        const button = document.createElement('button');
-        button.innerText = 'Zoom';
-        button.addEventListener('click', () => zoom(this.data));
-        document.body.appendChild(button);
-        this.svg.call(zoom(this.data));
-    }
-
-    public draw(): void {
         this.addClasses();
         this.addBasePairs();
         this.addResidues();
         this.addLabels();
+    }
+
+    public addZoom() {
+        //const button = document.createElement('button');
+        //button.innerText = 'Zoom';
+        //button.addEventListener('click', () => zoom(this.data));
+        //document.body.appendChild(button);
+        this.canvas.call(zoom(this.data));
+    }
+
+    public draw(): void {
+        const context = this.canvas.node().getContext('2d');
+        context.fillStyle = '#fff';
+        context.rect(0, 0, +this.canvas.attr('width'), +this.canvas.attr('height'));
+        context.fill();
+
+        this.drawBasePairs();
+        this.drawResidues();
+        this.drawLabels();
     }
 
     private setDimensions(): void {
@@ -43,7 +54,7 @@ export class RNAVis {
         }
         width += this.round(2 * this.margin);
         height += this.round(2 * this.margin);
-        this.svg.attr('width', width)
+        this.canvas.attr('width', width)
         .attr('height', height);
     }
 
@@ -72,7 +83,7 @@ export class RNAVis {
             });
             styles += '\n}';
         });
-        this.svg.append('style')
+        this.canvas.append('style')
         .attr('type', 'text/css')
         .text(styles);
     }
@@ -83,11 +94,11 @@ export class RNAVis {
         const bpX = (resIndex: number) => this.formCoor(rna.sequence[resIndex].x);
         const bpY = (resIndex: number) => this.formCoor(rna.sequence[resIndex].y);
 
-        const bps = this.svg.append('g').attr('class', 'bps');
+        // const bps = this.canvas.append('g').attr('class', 'bps');
 
-        bps.selectAll('line')
+        this.dataContainer.selectAll('custom.bp-line')
         .data(rna.basePairs)
-        .join('line')
+        .join('custom')
         .attr('x1', (bp: BasePair) => bpX(bp.residueIndex1))
         .attr('y1', (bp: BasePair) => bpY(bp.residueIndex1))
         .attr('x2', (bp: BasePair) => bpX(bp.residueIndex2))
@@ -97,10 +108,11 @@ export class RNAVis {
 
     private addResidues(): void {
         const sequenceData = this.data.rnaComplexes[0].rnaMolecules[0].sequence;
-        const residues = this.svg.append('g').attr('class', 'residues');
+        //const residues = this.canvas.append('g').attr('class', 'residues');
 
         const getX = (res: Residue) => this.formCoor(res.x);
         const getY = (res: Residue) => this.formCoor(res.y);
+
         const addLines = () => {
             type Line = {res1: Residue, res2: Residue};
             let lineData: Line[] = [];
@@ -110,9 +122,9 @@ export class RNAVis {
                     res2: sequenceData[i], 
                 });
             }
-            residues.selectAll('line')
+            this.dataContainer.selectAll('custom.res-line')
             .data(lineData)
-            .join('line')
+            .join('custom')
             .attr('x1', (line: Line) => getX(line.res1))
             .attr('y1', (line: Line) => getY(line.res1))
             .attr('x2', (line: Line) => getX(line.res2))
@@ -120,27 +132,25 @@ export class RNAVis {
             .attr('class', 'bp-line res-line');
         }
         const addCircles = () => {
-            residues.selectAll('circle')
+            this.dataContainer.selectAll('custom.res-circle')
             .data(sequenceData)
-            .join('circle')
+            .join('custom')
             .attr('cx', getX)
             .attr('cy', getY)
             .attr('r', this.round(this.getFontSize(this.data.classes) * 0.75))
-            .attr('class', 'residue-circle');
+            .attr('class', 'res-circle');
         }
         const addTitles = () => {
             const resTitle = (residue: Residue) => 
             `${residue.residueIndex} (position.label in template: ${residue.residueIndex}.${residue.residueName}\')`;
-            residues.selectAll('g')
+            this.dataContainer.selectAll('custom.res-type')
             .data(sequenceData)
-            .join('g')
-            .append('title')
-            .text(resTitle);
-            residues.selectAll('g').append('text')
+            .join('custom')
+            .attr('title', resTitle)
             .attr('x', getX)
             .attr('y', getY)
-            .attr('class', (residue: Residue) => residue.classes.join(' '))
-            .text((residue: Residue) => residue.residueName);
+            .attr('class', (residue: Residue) => residue.classes.join(' ') + ' res-type')
+            .attr('text', (residue: Residue) => residue.residueName);
         }
 
         addLines();
@@ -150,23 +160,92 @@ export class RNAVis {
 
     private addLabels(): void {
         const labelData = this.data.rnaComplexes[0].rnaMolecules[0].labels;
-        const labels = this.svg.append('g').attr('class', 'labels');
 
-        labels.selectAll('g')
+        this.dataContainer.selectAll('custom.label')
         .data(labelData)
-        .join('g')
+        .join('custom')
         .attr('class', 'label')
-        .append('text')
-        .text((label: Label) => label.labelContent.label)
-        .attr('x', (label: Label) => this.formCoor(label.labelContent.x))
-        .attr('y', (label: Label) => this.formCoor(label.labelContent.y))
-        .attr('class', (label: Label) => label.labelContent.classes.join(' '));
-        labels.selectAll('g').append('line')
+        .attr('text', (label: Label) => label.labelContent.label)
+        .attr('lx', (label: Label) => this.formCoor(label.labelContent.x))
+        .attr('ly', (label: Label) => this.formCoor(label.labelContent.y))
         .attr('x1', (label: Label) => this.formCoor(label.labelLine.x1))
         .attr('x2', (label: Label) => this.formCoor(label.labelLine.x2))
         .attr('y1', (label: Label) => this.formCoor(label.labelLine.y1))
-        .attr('y2', (label: Label) => this.formCoor(label.labelLine.y2))
-        .attr('class', (label: Label) => label.labelLine.classes.join(' '));
+        .attr('y2', (label: Label) => this.formCoor(label.labelLine.y2));
+    }
+
+    private drawBasePairs(): void {
+        const context = this.canvas.node().getContext('2d');
+
+        this.dataContainer
+        .selectAll('custom.bp-line')
+        .each(function(d) {
+            const node = d3.select(this);
+
+            // TODO: dynamically
+            context.strokeStyle = 'black';
+            context.lineWidth = 0.5;
+
+            context.beginPath();
+            context.moveTo(+node.attr('x1'), +node.attr('y1'));
+            context.lineTo(+node.attr('x2'), +node.attr('y2'));
+            context.stroke();
+        });
+    }
+
+    private drawResidues(): void {
+        const context = this.canvas.node().getContext('2d');  
+
+        this.dataContainer
+        .selectAll('custom.res-circle')
+        .each(function(d) {
+            const node = d3.select(this);
+
+            // TODO: dynamically
+            context.fillStyle = 'white';
+            context.beginPath();
+            context.arc(+node.attr('cx'), +node.attr('cy'), +node.attr('r'), 0, 2 * Math.PI);
+            context.fill();
+        });
+        this.dataContainer
+        .selectAll('custom.res-type')
+        .each(function(d) {
+            const node = d3.select(this);
+
+            // TODO: dynamically
+            context.font = '7.58px Helvetica';
+            context.fillStyle = 'black';
+
+            context.textAlign = 'center';
+            context.textBaseline = 'middle';
+            context.fillText(node.attr('text'), +node.attr('x'), +node.attr('y'));
+        });
+    }
+
+    private drawLabels(): void {
+        const context = this.canvas.node().getContext('2d');  
+
+         this.dataContainer
+        .selectAll('custom.label')
+        .each(function(d) {
+            const node = d3.select(this);
+
+            // TODO: dynamically
+            context.font = '7.58px Helvetica';
+            context.fillStyle = 'black';
+
+            context.textAlign = 'center';
+            context.textBaseline = 'middle';
+            context.fillText(node.attr('text'), +node.attr('lx'), +node.attr('ly'));
+
+            context.strokeStyle = 'black';
+            context.lineWidth = 0.5;
+
+            context.beginPath();
+            context.moveTo(+node.attr('x1'), +node.attr('y1'));
+            context.lineTo(+node.attr('x2'), +node.attr('y2'));
+            context.stroke();
+        });
     }
 }
 
