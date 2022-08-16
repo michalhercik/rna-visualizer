f3813fd691a3e5f783873ab8feb84aa56a3f7496
Michal Hercík <hercik77@seznam.cz>
2022-08-16 13:28:15 +0200

    parameters of a render function moved to a constructor

--- src/draw.ts
+++ src/draw.ts
@@ -3,37 +3,45 @@ import { Styles } from './classes';
 
 abstract class DrawTemplate {
     protected styles;
+    protected classComb;
+    protected context;
+    protected dataContainer
 
-    public constructor(style: Styles) {
+    public constructor(style: Styles, 
+                       context: CanvasRenderingContext2D, 
+                       classComb: Set<unknown>, 
+                       dataContainer: any) {
         this.styles = style;
+        this.classComb = classComb;
+        this.context = context;
+        this.dataContainer = dataContainer;
     }
 
-    public draw(context: CanvasRenderingContext2D, classComb: Set<unknown>, dataContainer: any) {
-        classComb.forEach((comb: string) => {
+    public draw() {
+        this.classComb.forEach((comb: string) => {
             const objectStyles = this.styles.get(comb)
-            this.setContext(context, objectStyles);
-            // TODO: probably better way to do this
-            const a = this.do;
-            dataContainer
-            .selectAll('[class="' + comb + '"]')
+            this.setContext(objectStyles);
+            const r = this.render;
+            const ctx = this.context;
+            this.dataContainer
+            .selectAll(`[class="${comb}"]`)
             .each(function() {
-                const node = d3.select(this);
-                a(context, node);
+                r(d3.select(this), ctx);
             });
         });
     }
 
-    protected abstract setContext(ctx: CanvasRenderingContext2D, objectStyles: any): void;
-    protected abstract do(context: CanvasRenderingContext2D, shape: object): void;
+    protected abstract setContext(objectStyles: any): void;
+    protected abstract render(shape: object, context: CanvasRenderingContext2D): void;
 }
 
 export class LinesDrawer extends DrawTemplate {
-    protected setContext(context: CanvasRenderingContext2D, objectStyles: any): void {
-        context.strokeStyle = objectStyles['stroke'] || 'black';
-        context.lineWidth = objectStyles['stroke-width'] || 1;
+    protected setContext(objectStyles: any): void {
+        this.context.strokeStyle = objectStyles['stroke'] || 'black';
+        this.context.lineWidth = objectStyles['stroke-width'] || 1;
     }
 
-    protected do(context: CanvasRenderingContext2D, shape: any): void {
+    protected render(shape: any, context: CanvasRenderingContext2D): void {
         context.beginPath();
         context.moveTo(+shape.attr('x1'), +shape.attr('y1'));
         context.lineTo(+shape.attr('x2'), +shape.attr('y2'));
@@ -42,13 +50,13 @@ export class LinesDrawer extends DrawTemplate {
 }
 
 export class CirclesDrawer extends DrawTemplate {
-    protected setContext(context: CanvasRenderingContext2D, objectStyles: any): void {
-        context.strokeStyle = objectStyles['stroke'] || 'black';
-        context.fillStyle = objectStyles['fill'] || 'white';
-        context.lineWidth = objectStyles['stroke-width'] || 1;
+    protected setContext(objectStyles: any): void {
+        this.context.strokeStyle = objectStyles['stroke'] || 'black';
+        this.context.fillStyle = objectStyles['fill'] || 'white';
+        this.context.lineWidth = objectStyles['stroke-width'] || 1;
     }
 
-    protected do(context: CanvasRenderingContext2D, shape: any): void {
+    protected render(shape: any, context: CanvasRenderingContext2D): void {
         context.beginPath();
         context.arc(+shape.attr('cx'), +shape.attr('cy'), +shape.attr('r'), 0, 2 * Math.PI);
         context.fill();
@@ -58,23 +66,34 @@ export class CirclesDrawer extends DrawTemplate {
 export class TextDrawer extends DrawTemplate {
     private translate = new Map();
 
-    public constructor(style: Styles) {
-        super(style);
+    public constructor(style: Styles,
+                       context: CanvasRenderingContext2D, 
+                       classComb: Set<unknown>, 
+                       dataContainer: any) {
+        super(style,
+             context,
+             classComb,
+             dataContainer);
         this.translate.set('start', 'left');
         this.translate.set('middle', 'center');
         this.translate.set('end', 'right');
     }
-    protected setContext(context: CanvasRenderingContext2D, objectStyles: any): void {
-        context.font = 
+    protected setContext(objectStyles: any): void {
+        const fontSize = () => {
+            const k = objectStyles['k'] || 1;
+            return objectStyles['font-size'].slice(0,-2) * k + 'px';
+        }
+        fontSize();
+        this.context.font = 
             (objectStyles['font-weight'] || 'normal') + ' ' + 
-            (objectStyles['font-size'] || '6px') + ' ' + 
+            (fontSize() || '6px') + ' ' + 
             (objectStyles['font-family'] || 'Helvetica');
-        context.fillStyle = objectStyles['fill'] || 'black';
-        context.textAlign = this.translate.get(objectStyles['text-anchor'] || 'middle');
-        context.textBaseline = objectStyles['baseline'] || 'middle';
+        this.context.fillStyle = objectStyles['fill'] || 'black';
+        this.context.textAlign = this.translate.get(objectStyles['text-anchor'] || 'middle');
+        this.context.textBaseline = objectStyles['baseline'] || 'middle';
     }
 
-    protected do(context: CanvasRenderingContext2D, shape: any): void {
+    protected render(shape: any, context: CanvasRenderingContext2D): void {
         context.fillText(shape.attr('text'), +shape.attr('x'), +shape.attr('y'));
     }
 }
